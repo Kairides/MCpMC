f229ad9a74e9ddd65a31d2944041bd295eff64af
fournier-p <fournier-p@port-lehn.lina.sciences.univ-nantes.prive>
2018-03-28 21:03:20 +0200

    running version

--- simumodules.py
+++ simumodules.py
@@ -1,64 +1,78 @@
-import modules
-import random
+""" Simulation of pMC with modules"""
+from random import random
+from modules import mysub
 
-def typennotexp(e):
-    return type(e)==int or type(e)==float
+def typennotexp(expr):
+    """return true if type is int or float"""
+    return isinstance(expr, (int, float))
 
-def sim(l,pmc):# l =length of exec, pmc = pmc to simulate
+
+def sim(length, pmc,valu=None):
+    """simulate pMC once for at most length step
+        return the accumulated reward"""
     pmc.reinit()
-    end=False
-    step=0
-    r=0
-    prob=1
-    while step<l and not end :
-        step+=1
-        t=pmc.getPos()
-        for i in range(0,len(t)):
-            if len(t[i])>1:
+    end = False
+    step = 0
+    cumu_reward = 0
+    prob = 1
+    while step < length and not end:
+        step += 1
+        numb_mod_deadlocked = 0
+        for trans_mod in pmc.get_possible_transitions():
+            if len(trans_mod) > 1:
                 raise Exception("several action possible")
-            elif len(t[i])>0:
-                tr=t[i][0]
-                name,cond,outcom=tr
-                norma=sum(not typennotexp(e[0]) for e in outcom)
-                if norma>0:
-                    np=1/norma
-                threshold=random.random()
-                c=0
-                j=0
-                while j<len(outcom) and c<threshold:
-                    v=outcom[j][0]
-                    if typennotexp(v):
-                        c+=v
+            if trans_mod:
+                if valu is None:
+                    name, _, outcom = trans_mod[0]
+                    norma = sum(not typennotexp(e[0]) for e in outcom)
+                    threshold = random()
+                    j = 0
+                    while j < len(outcom) and threshold > 0:
+                        if typennotexp(outcom[j][0]):
+                            threshold -= outcom[j][0]
+                        else:
+                            threshold -= 1/norma
+                        j += 1
+                    j -= 1
+                    realprob = outcom[j][0]
+                    if typennotexp(realprob):
+                        prob *= mysub(realprob, pmc.get_valuation())/realprob
                     else:
-                        c+=np
-                    j+=1
-                j-=1
-                realprob=outcom[j][0]
-                if typennotexp(realprob):
-                    unifprob=realprob
+                        prob *= mysub(realprob, pmc.get_valuation())*norma
+                    pmc.maj(outcom[j][1])
+                    cumu_reward += pmc.get_reward(name)
                 else:
-                    unifprob=np
-                prob*=realprob/unifprob
-#TODO metre a jour les etats
-                
-                r+=pmc.rewardAct(name)
-                r+=pmc.rewardState()
+                    name, _, outcom = trans_mod[0]
+                    threshold = random()
+                    j = 0
+                    while j < len(outcom) and threshold > 0:
+                        threshold -= mysub(mysub(outcom[j][0],valu),pmc.get_valuation())
+                        j += 1
+                    j -= 1
+                    realprob = outcom[j][0]
+                    prob *= mysub(realprob, pmc.get_valuation())/mysub(mysub(outcom[j][0],valu),pmc.get_valuation())
+                    pmc.maj(outcom[j][1])
+                    cumu_reward += pmc.get_reward(name)
             else:
-                end=True
+                numb_mod_deadlocked += 1
+            end = (numb_mod_deadlocked == len(pmc.modules))
+                #if end:
+#print("end at l = "+str(step))
+#print(cumu_reward)
+    return prob*cumu_reward
 
-    return prob*r
 
 
+def simu(length, num_simu, pmc,valu=None):
+    """length = length of exec, num_simu = number of simu, pmc = pmc to simulate"""
+    accu_reward = 0
+    accu_var = 0
+    for _ in range(0, num_simu):
+        #print("sim # = "+str(i))
+        random_var_y = sim(length, pmc,valu)
+        accu_reward += random_var_y
+        accu_var += random_var_y*random_var_y
 
-def simu(l,n,pmc): # l = length of exec, n = number of simu, pmc = pmc to simulate
-    r=0
-    v=0
-    for i in range(0,n):
-        print(i)
-        y=sim(l,pmc)
-        r+=y
-        v+=y*y
-
-    estespy=r/n
-    estvar=(v/(n-1)-n/(n-1)*estespy**2)**(1/2)
-    return [estespy,estvar]
+    estespy = accu_reward/num_simu
+    estvar = (accu_var/(num_simu-1)-num_simu/(num_simu-1)*estespy**2)**(1/2)
+    return [estespy, estvar]
